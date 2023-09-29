432dc1cd253735d7e1336d7addf8b096b8dd2d56
Chacoon3 <dyzily@foxmail.com>
2023-09-28 22:38:00 -0400

    commit

---
+++ dist/assets/LoadingView-207b7d49.js
@@ -0,0 +1,1 @@
+import{_ as o,o as _,c as a,p as s,a as t,b as c}from"./index-3cf7f63b.js";const d={},n=e=>(s("data-v-a0591482"),e=e(),t(),e),i={id:"loadingDiv"},p=n(()=>c("h1",null,"App Loading...",-1)),r=[p];function l(e,h){return _(),a("div",i,r)}const f=o(d,[["render",l],["__scopeId","data-v-a0591482"]]);export{f as default};
